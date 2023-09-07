01377d79c57d9d5a54317e567f9123608fd9b41a
KurtReti <kurt.reti@gmail.com>
2023-09-07 14:03:41 +1000

    math fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import './App.css';
-import DilutionInput from './DilutionInput';
+import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [dilutionLeft, setDilutionLeft] = useState(1);
+  const [dilutionRight, setDilutionRight] = useState(1);
+  const [ratio, setRatio] = useState();
+  const [remainder, setRemainder] = useState();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "left") {
+      setDilutionLeft(parseInt(value));
+    } else {
+      setDilutionRight(parseInt(value));
+    }
+  };
+
+  useEffect(() => {
+    calculateRatio(dilutionLeft, dilutionRight);
+    displayResult();
+  }, [dilutionLeft, dilutionRight]);
+
+  const calculateRatio = (left, right) => {
+    let ratio = ((1 / (left + right)) * 100).toFixed(2);
+    let remainder = (100 - ratio).toFixed(2);
+    if (!isNaN(ratio)) {
+      setRemainder(remainder);
+      setRatio(ratio);
+    }
+  };
+
+  const displayResult = () => {};
+
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <DilutionInput />
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="border w-full h-1/5 border-black absolute bottom-0">
+        <input
+          name="left"
+          type="text"
+          className="h-full pr-1 text-right text-md select-text placeholder-slate-300"
+          defaultValue="1"
+          maxLength={4}
+          onChange={handleChange}
+        ></input>
+        <span>:</span>
+        <input
+          name="right"
+          type="text"
+          className="h-full pr-1 text-left text-md select-text placeholder-slate-300"
+          defaultValue="1"
+          maxLength={4}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div>{ratio + "% " + remainder + "% "}</div>
     </div>
   );
 }
